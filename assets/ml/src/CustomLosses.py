ce0bb83dc0f5e36edc58d2b0425c351ec592f023
Brad Rice <bkr11a@gmail.com>
2024-01-06 16:07:48 +0800

    Addition of Mean Angular Error

--- assets/ml/src/CustomLosses.py
+++ assets/ml/src/CustomLosses.py
@@ -37,3 +37,20 @@ class EPE_Loss(tf.keras.losses.Loss):
         epe = tf.reduce_mean(u_epe + v_epe)
 
         return epe
+
+class AE_Loss(tf.keras.losses.Loss):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    @tf.function
+    def call(self, y_true, y_pred):
+        u_true = y_true[:, :, :, 0]
+        u_pred = y_pred[:, :, :, 0]
+
+        v_true = y_true[:, :, :, 1]
+        v_pred = y_pred[:, :, :, 1]
+        
+        angularError = tf.math.acos(( u_pred * u_true + v_pred * v_true +1.0)) / tf.sqrt( (tf.square(u_pred) + tf.square(v_pred) + 1.0)*(tf.square(u_true) + tf.square(v_true) + 1.0) )
+        angularError = tf.reduce_mean(angularError)
+
+        return angularError
